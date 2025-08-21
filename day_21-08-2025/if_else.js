3fe9b0a7d80f4dd11f0c5138c8ee3bcc214106b7
Rishi <baburishi505@gmail.com>
2025-08-21 22:56:08 +0530

    Day 02: JS - if/else basics + gradeChecker

---
+++ day_21-08-2025/if_else.js
@@ -0,0 +1,19 @@
+//a =parseInt(prompt("enter your grade: "));
+a=75;
+console.log(a);
+
+if(a>=90){
+    console.log("you got A grade")
+}
+else if(a>=80){
+    console.log("your got B grade")
+}
+else if(a>=70){
+    console.log("your got C grade")
+}
+else if(a>=60){
+    console.log("your got D grade")
+}
+else{
+    console.log("your got F grade")
+}
